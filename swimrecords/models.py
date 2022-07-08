5a24454fe7e945b2510ef0c31b9ab5a14ddd8851
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-07-07 23:01:15 -0400

    first

--- swimrecords/models.py
+++ swimrecords/models.py
@@ -1,12 +1,44 @@
+from datetime import timedelta
+from django.utils import timezone
+from logging import raiseExceptions
+from xml.dom import ValidationErr
+from django.forms import ValidationError
 from django.db import models
 
+
+def relay_check(stroke):
+    # 'front crawl', 'butterfly', 'breast', 'back', or 'freestyle
+    stroke_list = ['front crawl', 'butterfly', 'breast', 'back', 'freestyle']
+    if stroke not in stroke_list:
+        raise ValidationError(f"{stroke} is not a valid stroke")
+
+
+def distance_check(distance):
+    limit = 50
+    if distance <= limit:
+        raise ValidationError(
+            "Ensure this value is greater than or equal to 50.")
+
+
+def date_check(date):
+    if date > timezone.now():
+        raise ValidationError("Can't set record in the future.")
+
+
+def record_broken_date_check(record_broken_date):
+    if record_broken_date < timezone.now():
+        raise ValidationError("Can't break record before record was set.")
+    return record_broken_date
+
+
 class SwimRecord(models.Model):
-    pass # delete me when you start writing in validations
-    # first_name = models.CharField()
-    # last_name = models.CharField()
-    # team_name = models.CharField()
-    # relay = models.BooleanField()
-    # stroke = models.CharField()
-    # distance = models.IntegerField()
-    # record_date = models.DateTimeField()
-    # record_broken_date = models.DateTimeField()
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    team_name = models.CharField(max_length=255)
+    relay = models.BooleanField(default=False)
+    stroke = models.CharField(max_length=255, validators=[relay_check])
+    distance = models.IntegerField(validators=[distance_check])
+    record_date = models.DateTimeField(validators=[date_check])
+    record_broken_date = models.DateTimeField(
+        validators=[record_broken_date_check])
